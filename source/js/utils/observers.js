a83aee4bde77cee03eb12ffa3674b68207e53997
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-06-03 14:08:09 +0300

    feat: добавит observer

---
+++ source/js/utils/observers.js
@@ -0,0 +1,31 @@
+export class EventObserver {
+  constructor() {
+    this.observers = [];
+    this.fire = this.fire.bind(this);
+  }
+
+  subscribe(fn) {
+    this.observers.push(fn);
+  }
+
+  unsubscribe(fn) {
+    this.observers = this.observers.filter((subscriber) => subscriber !== fn);
+  }
+
+  fire(data) {
+    this.observers.forEach((subscriber) => subscriber(data));
+  }
+}
+
+export const resizeObserver = new EventObserver();
+
+const resizeObserverProto = new ResizeObserver(() =>
+  setTimeout(() => resizeObserver.fire('resize'), 10)
+);
+
+resizeObserverProto.observe(document.documentElement);
+
+// везде следует использовать его прим: resizeObserver.subscribe(() => {console.log('asd')})
+
+export const clickObserver = new EventObserver();
+window.addEventListener('click', clickObserver.fire);
